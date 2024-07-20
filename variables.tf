b103f6abea9d1b5c914df0866644d779d6bcc654
Alan Peh <161850288+lann87@users.noreply.github.com>
2024-07-20 13:03:02 +0800

    Update variables2.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "ami_id" {
 variable "ec2_name" {
   description = "Name of EC2"
   type        = string
-  default     = "my-sample-ec2-luqman-from-tf" # Change accordingly
+  default     = "alanp-ec2-date-tf" # Change accordingly
 }
 
 variable "instance_type" {
@@ -19,23 +19,23 @@ variable "instance_type" {
 variable "key_name" {
   description = "Name of EC2 Key Pair"
   type        = string
-  default     = "luqman-useast1-13072024" # Change accordingly
+  default     = "alan_ce_useast1" # Change accordingly
 }
 
 variable "sg_name" {
   description = "Name of EC2 security group"
   type        = string
-  default     = "luqman-ec2-allow-ssh-http-https" # Change accordingly
+  default     = "alanp-ec2-allow-ssh-http-https" # Change accordingly
 }
 
 variable "vpc_name" {
   description = "Name of VPC to use"
   type        = string
-  default     = "luqman-vpc" # Change accordingly
+  default     = "alanp-vpc" # Change accordingly
 }
 
 variable "subnet_name" {
   description = "Name of subnet to use"
   type        = string
-  default     = "luqman-subnet-public1-us-east-1a" # Change accordingly
+  default     = "alanp-subnet-public1-us-east-1a" # Change accordingly
 }
